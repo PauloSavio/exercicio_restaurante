ee003924171ad8c33836cbb88f60fafc586e9882
Savio <pauloluizsavio@gmail.com>
2021-09-29 11:39:17 -0300

    Primeiro teste

--- src/br/com/zup/Ingrediente.java
+++ src/br/com/zup/Ingrediente.java
@@ -1,9 +1,24 @@
 package br.com.zup;
 
 public class Ingrediente {
+    private String nomeDoIngrediente;
 
-    public Ingrediente() {
+    public Ingrediente(String nomeDoIngrediente) {
+        this.nomeDoIngrediente = nomeDoIngrediente;
     }
 
+    public String getNomeDoIngrediente() {
+        return nomeDoIngrediente;
+    }
 
+    public void setNomeDoIngrediente(String nomeDoIngrediente) {
+        this.nomeDoIngrediente = nomeDoIngrediente;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append(nomeDoIngrediente);
+        return retorno.toString();
+    }
 }
--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -3,6 +3,16 @@ package br.com.zup;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Prato prato1 = new Prato("Feijoada", 50);
+
+        Ingrediente ingrediente1 = new Ingrediente("Feijão");
+        Ingrediente ingrediente2 = new Ingrediente("Carne");
+
+
+        System.out.println(prato1);
+        prato1.adicionarIngrediente(ingrediente1);
+        prato1.adicionarIngrediente(ingrediente2);
+        System.out.println("-------------------");
+        System.out.println(prato1);
     }
 }
